c9a7291d7179e0d32540d1002b79465c6feeac7e
Fede Rozenberg <fede@tendrel.io>
2024-08-07 08:27:28 -0700

    chore: only do metro shenaningans in dev

--- metro.config.js
+++ metro.config.js
@@ -4,25 +4,27 @@ const path = require("node:path");
 
 const config = getDefaultConfig(__dirname);
 
-const localSdkPath = path.resolve(__dirname, "../sdk");
-const extraNodeModules = {
-  "@tendrel/sdk": localSdkPath,
-};
-
-config.resolver.sourceExts.push("sql");
-
-config.resolver.extraNodeModules = {
-  "@tendrel/sdk": localSdkPath,
-};
-
-config.resolver.extraNodeModules = new Proxy(extraNodeModules, {
-  get: (target, name) =>
-    // redirects dependencies referenced from myExtraModule/ to local node_modules
-    name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`),
-});
-config.resetCache = true;
-config.watchFolders = [localSdkPath];
-config.resolver.unstable_enableSymlinks = true;
-config.resolver.unstable_enablePackageExports = true;
+// Uncomment the following block to develop locally with the sdk
+// const localSdkPath = path.resolve(__dirname, "../sdk");
+// const extraNodeModules = {
+//   "@tendrel/sdk": localSdkPath,
+// };
+//
+// config.resolver.sourceExts.push("sql");
+//
+// config.resolver.extraNodeModules = {
+//   "@tendrel/sdk": localSdkPath,
+// };
+//
+// config.resolver.extraNodeModules = new Proxy(extraNodeModules, {
+//   get: (target, name) =>
+//     // redirects dependencies referenced from myExtraModule/ to local node_modules
+//     name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`),
+// });
+// config.resetCache = true;
+// config.watchFolders = [localSdkPath];
+// config.resolver.unstable_enableSymlinks = true;
+// config.resolver.unstable_enablePackageExports = true;
+// ** end of block **
 
 module.exports = config;
