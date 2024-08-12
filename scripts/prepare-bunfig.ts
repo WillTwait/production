2cb566f5f50f987c1e8ed4fc6132ca38755dc19b
Fede Rozenberg <fede@tendrel.io>
2024-08-12 09:31:33 -0700

    chore: (initial) maestro tests via github actions (#8)

---
+++ scripts/prepare-bunfig.ts
@@ -0,0 +1,11 @@
+import { $ } from "bun";
+
+const bunfigContent = `[install]
+registry = { token = \"${process.env.NPM_TOKEN}\", url = \"https://registry.npmjs.com/\" }`;
+
+await $`echo "Creating bunfig.toml..."`;
+await $`rm -f bunfig.toml`;
+await $`echo ${bunfigContent} > bunfig.toml`;
+await $`echo "Bunfig created. Running installation..."`;
+await $`bun i`;
+await $`echo "Installation complete."`;
