b2958bf4910ff56b7fbc984847f272e6d5df739b
John Murphy <murphy@jhm4.com>
2024-10-10 08:55:04 -0500

    fix: theme provider (#38)

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,16 +1,25 @@
+import { useTheme } from "@/hooks/useTheme";
 import { RelayProvider } from "@/relay/provider";
 import { TendrelProvider } from "@/tendrel/provider";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
 
 const AppEntryLayout = () => {
+  const { colorTheme } = useTheme();
   return (
-    <RelayProvider url={process.env.EXPO_PUBLIC_TENDREL_GRAPHQL_URL}>
-      <TendrelProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </TendrelProvider>
-    </RelayProvider>
+    <ThemeProvider value={colorTheme === "dark" ? DarkTheme : DefaultTheme}>
+      <RelayProvider url={process.env.EXPO_PUBLIC_TENDREL_GRAPHQL_URL}>
+        <TendrelProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </TendrelProvider>
+      </RelayProvider>
+    </ThemeProvider>
   );
 };
 
